ff25632fd18c7ab792dc8a6024e791c66af73221
gerynsb <118332285+gerynsb@users.noreply.github.com>
2024-11-11 08:17:08 +0800

    Update : next day

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { NAV_LINKS } from '@/constants'
+import Button from './Button'
 
 const Navbar = () => {
   return (
@@ -13,12 +14,30 @@ const Navbar = () => {
       <ul className='hidden h-full gap-12 lg:flex'>
         {NAV_LINKS.map((link) => (
           <li key={link.href}>
-            <Link href={link.href}>
+            <Link href={link.href} className='regular-16 text-gray-50 flexCenter
+            cursor-pointer pb-1.5 transition-all hover:font-bold'>
               {link.label}
             </Link>
           </li>
         ))}
       </ul>
+
+      <div className='lg:flexCenter hidden'>
+        <Button 
+            type="button"
+            title="Login"
+            icon="/user.svg"
+            variant="btn_dark_green"
+        />
+      </div>
+
+      <Image 
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className = "inline-block cursor-pointer lg:hidden"
+      />
     </nav>
   )
 }
